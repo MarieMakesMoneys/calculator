c924dd13daae5fd08f51912bebff8213233fccd2
MarieMakesMoneys <marie.collecchia@gmail.com>
2022-10-13 17:04:56 +0200

    Query all buttons and output divs

--- jscript.js
+++ jscript.js
@@ -1,1 +1,24 @@
+const C = document.querySelector('.C');
+const posneg = document.querySelector('.posneg');
+const modulo = document.querySelector('.modulo');
+const DEL = document.querySelector('.DEL');
+const seven = document.querySelector('.7');
+const eight = document.querySelector('.8');
+const nine = document.querySelector('.9');
+const divide = document.querySelector('.divide');
+const four = document.querySelector('.4');
+const five = document.querySelector('.5');
+const six = document.querySelector('.6');
+const mult = document.querySelector('.mult');
+const one = document.querySelector('.1');
+const two = document.querySelector('.2');
+const three = document.querySelector('.3');
+const minus = document.querySelector('.minus');
+const zero = document.querySelector('.0');
+const point = document.querySelector('.point');
+const equal = document.querySelector('.equal');
+const plus = document.querySelector('.plus');
+const oldLine = document.querySelector('.toptext');
+const typeLine = document.querySelector('.bottomtext');
+
 
