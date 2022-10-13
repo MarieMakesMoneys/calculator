bfe66467f2c2a3bd38c916ec13e4872848155367
MarieMakesMoneys <marie.collecchia@gmail.com>
2022-10-13 17:53:35 +0200

    Attempts

--- jscript.js
+++ jscript.js
@@ -1,24 +1,34 @@
 const C = document.querySelector('.C');
 const posneg = document.querySelector('.posneg');
-const modulo = document.querySelector('.modulo');
+const modulo = document.querySelector('#modulo');
 const DEL = document.querySelector('.DEL');
-const seven = document.querySelector('.7');
-const eight = document.querySelector('.8');
-const nine = document.querySelector('.9');
-const divide = document.querySelector('.divide');
-const four = document.querySelector('.4');
-const five = document.querySelector('.5');
-const six = document.querySelector('.6');
-const mult = document.querySelector('.mult');
-const one = document.querySelector('.1');
-const two = document.querySelector('.2');
-const three = document.querySelector('.3');
-const minus = document.querySelector('.minus');
-const zero = document.querySelector('.0');
-const point = document.querySelector('.point');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const divide = document.querySelector('#divide');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const mult = document.querySelector('#mult');
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const minus = document.querySelector('#minus');
+const zero = document.querySelector('#zero');
+const point = document.querySelector('#point');
 const equal = document.querySelector('.equal');
-const plus = document.querySelector('.plus');
+const plus = document.querySelector('#plus');
 const oldLine = document.querySelector('.toptext');
 const typeLine = document.querySelector('.bottomtext');
+const btns = document.querySelectorAll('.btn');
 
+five.addEventListener('click', () => {
+    console.log('five');
+    document.createElement('div').textContent = '5';
+    'typeLine'.appendChild('div');
+    console.log('victoire')
+});
 
+six.addEventListener('click', () => {
+    console.log('6')
+});
