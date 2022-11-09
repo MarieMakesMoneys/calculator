6f7985615e42abf831c79e372858f5dd80099cab
MarieMakesMoneys <marie.collecchia@gmail.com>
2022-11-09 16:01:46 +0100

    Revise calculations features to fix bugs

--- jscript.js
+++ jscript.js
@@ -9,73 +9,78 @@ let currentLine = document.querySelector('.bottomtext');
 const operators = document.querySelectorAll('.operator')
 const numbers = document.querySelectorAll('.num');
 
-let a = '0';
+let a = '';
 let b = '';
 let ope = '';
 
-numbers.forEach((number) => number.addEventListener('click', function (e) {
-    handleNumber(e.target.textContent);
-    currentLine.textContent = b;
-}));
+numbers.forEach((number) => {
+    number.addEventListener('click', function (e) {
+        handleNumber(e.target.textContent);
+        currentLine.textContent = b;
+        divB.textContent = b
+    })
+});
 
 function handleNumber(num) {
     b += num;
 };
 
-operators.forEach((operator) => operator.addEventListener('click', function(e) {
-    oldLine.textContent = b + ope; 
-    a = b;
-    b = '';
-    if (ope == '') {
-        handleOperator(e.target.textContent);
-        currentLine.textContent += ope; 
-    } else if (ope !== '') {
-        handleEqual();
-        handleOperator(e.target.textContent);
-        currentLine.textContent += ope; 
-    }
-}))
+let divA = document.querySelector('.a')
+let divB = document.querySelector('.b')
+let divC = document.querySelector('.c')
+let divOpe = document.querySelector('.ope')
+let divRes = document.querySelector('.res')
 
-function handleOperator(oper) {
-    ope = oper;
-}
+operators.forEach((operator) => { 
+    operator.addEventListener('click', function(e) {
+        if (a=='') {
+            ope = e.target.textContent
+            divOpe.textContent = ope
+            let c = b + ope; 
+            divC.textContent = c
+            a = b;
+            divA.textContent = a
+            b = '';
+            divB.textContent = b
+            oldLine.textContent = c
+        } else {
+            ope = e.target.textContent
+            divOpe.textContent = ope
+            let c = a + ope
+            divC.textContent = c
+            b = ''
+            divB.textContent = b
+            divA.textContent = a
+            oldLine.textContent = c
+        }
+        
+        //} else if (ope !== '') {
+        //    handleEqual();
+        //    handleOperator(e.target.textContent);
+        //    currentLine.textContent += ope; 
+        //}
+    })
+});
 
-function handleEqual() {
-    a = Number(a);
-    b = Number(b);
-    if (ope == '') {
-        oldLine.textContent = a + ope + b;
-        ope = '+';
-        let result = operate(a, b, ope);
-        ope = '';
-        b = Number(result.toFixed(3));
-        currentLine.textContent = b;
-    } else {
-        oldLine.textContent = a + ope + b;
-        let result = operate(a, b, ope);
-        ope = '';
-        b = Number(result.toFixed(3));
-        currentLine.textContent = b;
-    }
-}
+//function handleOperator(oper) {
+//    ope = oper;
+//}
+
+
 
 equal.addEventListener('click', function() {
     a = Number(a);
     b = Number(b);
-    if (ope == '') {
-        oldLine.textContent = a + ope + b;
-        ope = '+';
-        let result = operate(a, b, ope);
-        ope = '';
-        b = Number(result.toFixed(3));
-        currentLine.textContent = b;
-    } else {
-        oldLine.textContent = a + ope + b;
-        let result = operate(a, b, ope);
-        ope = '';
-        b = Number(result.toFixed(3));
-        currentLine.textContent = b;
-    }
+
+    oldLine.textContent = a + ope + b;
+    let result = operate(a, b, ope);
+    divRes.textContent = result
+    //ope = '';
+    
+   
+    a = Number(result.toFixed(3));
+     divA.textContent = a
+    currentLine.textContent = a;
 });
 
 function add(a, b) {
